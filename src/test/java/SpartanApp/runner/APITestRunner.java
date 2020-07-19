64870efd5e828470b9d2be7ff046bde50e60c5e1
furkantan-java <furkan.tan@windowslive.com>
2020-07-19 11:22:37 -0400

    update

--- src/test/java/SpartanApp/runner/APITestRunner.java
+++ src/test/java/SpartanApp/runner/APITestRunner.java
@@ -6,8 +6,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src\\test\\resources\\features\\api",
-        glue = "SpartanApp\\step_definitions",
+        features = "src/test/resources/features/api",
+        glue = "SpartanApp/step_definitions",
         dryRun = false,
         strict = false,
         tags = "",
